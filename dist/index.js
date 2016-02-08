5e60502aef2f5b5fcbdc3c0fd1eb65358c40dcbe
Darin Reid <darinreid@gmail.com>
2016-02-07 20:33:20 -0800

    Building

---
+++ dist/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var highlight = require('highlight.js');
+var nunjucks = require('nunjucks');
+
+var Inspect = function Inspect() {
+    this.tags = ['inspect'];
+
+    this.parse = function (parser, nodes, lexer) {
+        var token = parser.nextToken();
+
+        var args = parser.parseSignature(null, true);
+        parser.advanceAfterBlockEnd(token.value);
+
+        return new nodes.CallExtension(this, 'run', args);
+    };
+
+    this.run = function (context, input, callback) {
+        var output = highlight.highlight('js', JSON.stringify(input));
+        return new nunjucks.runtime.SafeString('<pre><code>' + output.value + '</code></pre>');
+    };
+};
+
+module.exports = Inspect;
