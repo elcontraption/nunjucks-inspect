470d1e10dc7eb9b0661e06fd83ba8a5a87aac295
Darin Reid <darinreid@gmail.com>
2016-02-07 19:55:29 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# nunjucks-inspect
+An `inspect` tag for Nunjucks, to inspect an object's properties.
+
+```html
+{% inspect obj %}
+```
---
+++ test.js
@@ -0,0 +1,23 @@
+const InspectExtension = require('./src');
+const nunjucks = require('nunjucks');
+const should = require('should');
+
+var env;
+
+before(function (cb) {
+    env = new nunjucks.Environment();
+    env.addExtension('inspect', new InspectExtension());
+    cb();
+});
+
+describe('inspect()', function () {
+    it('should render a string that includes inspected object property keys and values.', function () {
+        var obj = { testPropKey1: 'testPropValue1', testPropKey2: 'testPropValue2' };
+        var res = env.renderString('{% inspect obj %}', { obj: obj });
+
+        for (prop in obj) {
+            res.indexOf(prop).should.be.greaterThan(-1);
+            res.indexOf(obj[prop]).should.be.greaterThan(-1);
+        }
+    });
+});
